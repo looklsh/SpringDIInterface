6d94f7d967a2f69daf7becacf7ad89c227b1a961
이승휘 <looklsh@hanmail.net>
2020-03-04 16:09:19 +0900

    springdiinterface

---
+++ src/main/java/com/javalec/ex/Pencil6B.java
@@ -0,0 +1,13 @@
+package com.javalec.ex;
+
+public class Pencil6B implements Pencil {
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("6B입니다");
+
+	}
+
+}
---
+++ src/main/java/com/javalec/ex/Pencil6BWithEraser.java
@@ -0,0 +1,13 @@
+package com.javalec.ex;
+
+public class Pencil6BWithEraser implements Pencil {
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("6B이고 지우개가 달려있습니다");
+
+	}
+
+}
